968b25bf45d483be481c38a6d286b774ece9b436
Annalisa <annalisadesantis770@gmail.com>
2020-10-12 16:21:33 +0200

    fine esercizio

---
+++ main.js
@@ -0,0 +1,17 @@
+// Nome utente
+
+var nome_utente = prompt('Qual è il tuo nome?');
+
+// Cognome utente
+
+var cognome_utente = prompt('Qual è il tuo cognome?');
+
+// Colore preferito
+
+var colore_utente = prompt('Qual è il tuo colore preferito?');
+
+// Numero Predefinito
+
+var numero = 20;
+
+document.getElementById('generatore').innerHTML = nome_utente + cognome_utente + colore_utente + numero;
